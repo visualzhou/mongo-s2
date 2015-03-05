60023ecf83bf7f0c9b98880c1d5100b34194fd57
Siyuan Zhou <siyuan.zhou@mongodb.com>
2015-03-04 20:34:11 -0500

    main_test geohash convertion.

--- src/main_test.cc
+++ src/main_test.cc
@@ -1,8 +1,24 @@
 #include "s2.h"
+#include "s2cellid.h"
+#include "s2cell.h"
+#include "s2latlng.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-  cout << S2::Origin() << endl;
+int main(int argc, char* argv[]) {
+    string hash_string = "0f022022";
+    if (argc > 0) {
+        hash_string = string(argv[1]);
+    }
+    S2CellId start = S2CellId::FromString(hash_string);
+    S2Cell cell(start);
+    for (int i = 0; i < 5; ++i) {
+        S2Point point = cell.GetVertex(i % 4);
+        S2LatLng ll(point);
+        double lat = ll.lat().degrees();
+        double lng = ll.lng().degrees();
+        cout << lng << " " << lat << endl;
+    }
 }
